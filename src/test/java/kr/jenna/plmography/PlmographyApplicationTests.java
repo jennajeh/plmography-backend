e3203f633af8e8963b215f383a546c87b6779110
jennajeh <jennajuneh@gmail.com>
2022-12-19 23:42:33 +0900

    프로젝트 세팅

---
+++ src/test/java/kr/jenna/plmography/PlmographyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.jenna.plmography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlmographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
